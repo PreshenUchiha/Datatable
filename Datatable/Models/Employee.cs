d7fd0c2d7cfe028603f07abcbe18073ed7dd2431
Preshen Reddy <PreshenR@hollywoodbets.net>
2023-08-09 08:53:47 +0200

    Add project files.

---
+++ Datatable/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Datatable.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Position { get; set; }
+
+        [Required]
+        public string Age { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+    }
+}
